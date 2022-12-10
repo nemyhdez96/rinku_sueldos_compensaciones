b0dec9f0f374fef9d8b3f6432a8c52a8778206ea
nehemiash <nehe.hdez.96@gmail.com>
2022-12-09 23:25:44 -0600

    configuracion de los static

--- rinku/settings.py
+++ rinku/settings.py
@@ -128,14 +128,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(f"{BASE_DIR}/empleados", 'static'),
     os.path.join(f"{BASE_DIR}/compensaciones", 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
